ffd0e1950d617f16706e24fade38eb30724b033f
sinancakmak20 <100401737+sinancakmak20@users.noreply.github.com>
2024-05-24 23:11:11 +0300

    sign in page adjusted

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,17 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="relative flex items-center justify-center min-h-screen">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url('/sign_in_background.jpg')` }}
+      >
+        <div className="absolute inset-0 backdrop-blur-md"></div>
+      </div>
+      <div className="relative bg-white p-8 rounded-lg shadow-lg">
+        <SignIn path="/sign-in" />
+      </div>
+    </div>
+  );
 }
